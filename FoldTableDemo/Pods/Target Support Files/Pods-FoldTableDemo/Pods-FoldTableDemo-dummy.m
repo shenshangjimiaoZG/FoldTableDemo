ba0ec9322c935cd94d90e639a001567d299d6168
huangping35 <huangping@me.com>
2018-11-29 16:02:56 +0800

    cocoapods
    remove cocoapods的文件，自己cocoapods 不要偷懒

--- FoldTableDemo/Pods/Target Support Files/Pods-FoldTableDemo/Pods-FoldTableDemo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FoldTableDemo : NSObject
-@end
-@implementation PodsDummy_Pods_FoldTableDemo
-@end
--- FoldTableDemo/Pods/Target Support Files/UIImage+ImageCompress/UIImage+ImageCompress-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_UIImage_ImageCompress : NSObject
-@end
-@implementation PodsDummy_UIImage_ImageCompress
-@end
